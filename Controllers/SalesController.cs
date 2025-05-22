98ecbf17135cdd08096235b2f0c10a00b110882b
Nilavaz <nilavazhagiselvaraj04@gmail.com>
2025-05-22 20:03:01 +0530

    Update SalesController.cs

--- Controllers/SalesController.cs
+++ Controllers/SalesController.cs
@@ -1,1 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using SalesAnalysisDbApp.Models;
+using SalesAnalysisDbApp.Services;
 
+namespace SalesAnalysisDbApp.Controllers
+{
+
+  [ApiController]
+  [Route("api/[controller]")]
+  public class SalesController:  ControllerBase
+  {
+  private readonly MongoDbService _mongoService;
+
+    public SalesController(MongoDbService mongoService)
+    {
+    _mongoService = mongoService;
+    }
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+    var sales = await _mongoService.GetSalesAsync();
+    return Ok(sales);
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(SalesRecord newRecord)
+    {
+    await _mongoService.CreateSaleAsync(newRecord);
+    return CreatedAtAction(nameof(GetAll), new { id = newRecord.Id}, newRecord);
+    }
+    [HttpGet("summary")]
+    public async Task<IActionResult> GetSummary()
+    {
+    var summary = await _mongoService.GetSalesSummaryAsync();
+    return Ok(summary);
+    }
+  }
+}
